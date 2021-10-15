107300dcbda2ff6e8c98dfe494db4f374e5faa44
VaniaToper <vanjaqwe58@gmail.com>
2021-10-15 18:06:00 +0200

    feat🔨: weather

--- js/date.js
+++ js/date.js
@@ -1,7 +1,11 @@
 const timeMomentum = document.querySelector('.time');
 const dateMomentum = document.querySelector('.date');
 const greeting = document.querySelector('.greeting__text');
+const greetingInput = document.querySelector('.greeting__name');
 const wrapper = document.querySelector('.wrapper');
+const slidePrev = document.querySelector('.slide-prev');
+const slideNext = document.querySelector('.slide-next');
+let randomNum;
 
 const getTimeOfDate = () => {
   const date = new Date();
@@ -21,23 +25,45 @@ const getTimeOfDate = () => {
   }
 };
 
-const getRandomNum = (max) => {
-  num = Math.ceil(Math.random() * max).toString();
-  return num;
-};
+const getRandomNum = (() => {
+  randomNum = Math.ceil(Math.random() * 20).toString();
+  randomNum = randomNum.padStart(2, '0');
+  return randomNum;
+})();
 
 const setBackground = () => {
   getTimeOfDate();
-  getRandomNum(20);
-  num = num.padStart(2, '0');
-  wrapper.style.background = `url("/assets/img/${part.toLowerCase()}/${num}.jpg") center/cover, rgba(0, 0, 0, 0.5)`;
+  const img = new Image();
+  img.src = `/assets/img/${part.toLowerCase()}/${randomNum}.jpg`;
+  img.addEventListener('load', () => {
+    wrapper.style.background = `url("/assets/img/${part.toLowerCase()}/${randomNum}.jpg") center/cover`;
+  });
 };
+
 setBackground();
+const getSlideNext = () => {
+  if (randomNum < 20) {
+    randomNum = (parseInt(randomNum) + 1).toString();
+    randomNum = randomNum.padStart(2, '0');
+  } else randomNum = '01';
+  setBackground();
+};
+
+const getSlidePrev = () => {
+  if (randomNum > 1) {
+    randomNum = (parseInt(randomNum) - 1).toString();
+    randomNum = randomNum.padStart(2, '0');
+  } else randomNum = '20';
+  setBackground();
+};
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
 
 const showTime = () => {
   const date = new Date();
   getTimeOfDate();
-  greeting.textContent = `Good ${part} ,`;
+  greeting.textContent = `Good ${part},`;
   const currentTime = date.toLocaleTimeString();
   const options = {
     month: 'long',
@@ -49,5 +75,15 @@ const showTime = () => {
   dateMomentum.textContent = currentDate;
   setTimeout(showTime, 1000);
 };
-
 showTime();
+function setLocalStorage() {
+  localStorage.setItem('name', greetingInput.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem('name')) {
+    greetingInput.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
---
+++ js/weather.js
@@ -0,0 +1,18 @@
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=404aa4276ea9b5257a6608fa41b63388&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+getWeather();
+city.addEventListener('change', () => {
+  getWeather()
+});
